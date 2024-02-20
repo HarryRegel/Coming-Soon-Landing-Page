cfbdfe6114db1c53ecb10aefceb3d5989a367ca4
Alfa Ceti <131510131+HarryRegel@users.noreply.github.com>
2024-02-20 08:28:03 -0800

    Update notify.php
    Signed-off-by: Alfa Ceti <131510131+HarryRegel@users.noreply.github.com>

--- forms/notify.php
+++ forms/notify.php
@@ -1,10 +1,8 @@
+/**
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+ 
+ 
+  
 
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'contact@example.com';
@@ -37,3 +35,4 @@
 
   echo $contact->send();
 ?>
+**/
